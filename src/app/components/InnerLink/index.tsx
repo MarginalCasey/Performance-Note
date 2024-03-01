bc7db818f459a1243a951379d1d5e0a15d8d4fb4
張文源 <c29429768@gmail.com>
2024-03-01 12:00:36 +0800

    Add components/InnerLink

---
+++ src/app/components/InnerLink/index.tsx
@@ -0,0 +1,16 @@
+import { ArrowLeftEndOnRectangleIcon } from "@heroicons/react/24/solid";
+import type { AnchorHTMLAttributes, FC } from "react";
+
+const InnerLink: FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <a {...props}>
+      <ArrowLeftEndOnRectangleIcon className="inline-block align-middle mr-1 mb-1 w-4 h-4" />
+      {children}
+    </a>
+  );
+};
+
+export default InnerLink;
