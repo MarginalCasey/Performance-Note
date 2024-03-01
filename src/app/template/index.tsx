49210584662d335b34d079c395e3b6ca98d386dd
張文源 <c29429768@gmail.com>
2024-03-01 21:36:23 +0800

    Separate theme from template

--- src/app/template/index.tsx
+++ src/app/template/index.tsx
@@ -1,52 +1,14 @@
 "use client";
 
+import theme from "@/app/theme";
 import { ThemeProvider } from "@material-tailwind/react";
 import type { FC } from "react";
 import { ReactNode } from "react";
 import Sidebar from "./Sidebar";
 
 const Template: FC<{ children: ReactNode }> = ({ children }) => {
-  const customTheme = {
-    list: {
-      styles: {
-        base: {
-          list: {
-            minWidth: "min-w-auto",
-            p: "py-2",
-            fontSize: "text-sm",
-          },
-          item: {
-            initial: {
-              color:
-                "text-gray-600 hover:text-black focus:text-black active:text-black",
-              weight: "font-medium",
-            },
-            selected: {
-              bg: "bg-blue-gray-50",
-              color: "text-black",
-              fontWeight: "font-semibold",
-            },
-          },
-        },
-      },
-    },
-    accordion: {
-      styles: {
-        base: {
-          header: {
-            initial: {
-              color: "text-gray-600",
-              fontSize: "fontSize",
-              fontWeight: "font-medium",
-            },
-          },
-        },
-      },
-    },
-  };
-
   return (
-    <ThemeProvider value={customTheme}>
+    <ThemeProvider value={theme}>
       <div className="flex h-screen">
         <Sidebar />
         <main className="p-4 flex-1 overflow-y-auto">
---
+++ src/app/theme.ts
@@ -0,0 +1,40 @@
+const theme = {
+  list: {
+    styles: {
+      base: {
+        list: {
+          minWidth: "min-w-auto",
+          p: "py-2",
+          fontSize: "text-sm",
+        },
+        item: {
+          initial: {
+            color:
+              "text-gray-600 hover:text-black focus:text-black active:text-black",
+            weight: "font-medium",
+          },
+          selected: {
+            bg: "bg-blue-gray-50",
+            color: "text-black",
+            fontWeight: "font-semibold",
+          },
+        },
+      },
+    },
+  },
+  accordion: {
+    styles: {
+      base: {
+        header: {
+          initial: {
+            color: "text-gray-600",
+            fontSize: "fontSize",
+            fontWeight: "font-medium",
+          },
+        },
+      },
+    },
+  },
+};
+
+export default theme;
