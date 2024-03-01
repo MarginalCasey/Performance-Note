f27509ac1937ed9b49986c6c01a4600c01a2f5d5
張文源 <c29429768@gmail.com>
2024-03-01 12:35:49 +0800

    Shows breakdown of lcp and highlight lcp element

--- src/app/components/LCP/index.tsx
+++ src/app/components/LCP/index.tsx
@@ -1,22 +1,53 @@
 "use client";
 import { useEffect, useState } from "react";
-import type { LCPMetric } from "web-vitals";
-import { onLCP } from "web-vitals";
+import type { LCPMetricWithAttribution } from "web-vitals";
+import { onLCP } from "web-vitals/attribution";
 
 export default function LCP() {
-  const [lcp, setLCP] = useState<LCPMetric>();
+  const [lcp, setLCP] = useState<LCPMetricWithAttribution>();
 
   useEffect(() => {
     onLCP(setLCP, { reportAllChanges: true });
   }, []);
+
+  useEffect(() => {
+    if (lcp?.attribution.element) {
+      const selector = lcp.attribution.element.replace(
+        /[[\]]/g,
+        (match) => `\\${match}`,
+      );
+      const element: HTMLElement | null = document.querySelector(selector);
+
+      if (element) {
+        element.classList.add("border-4", "border-red-500");
+
+        return () => {
+          element.classList.remove("border-4", "border-red-500");
+        };
+      }
+    }
+  }, [lcp?.attribution.element]);
 
   return (
     <div>
       <h1>LCP</h1>
       {lcp && (
         <>
-          <div>value: {lcp.value}</div>
+          <div>value: {Math.round(lcp.value)}</div>
           <div>rating: {lcp.rating}</div>
+          <div>
+            Time to first byte: {Math.round(lcp.attribution.timeToFirstByte)}
+          </div>
+          <div>
+            Resource load delay: {Math.round(lcp.attribution.resourceLoadDelay)}
+          </div>
+          <div>
+            Resource load time: {Math.round(lcp.attribution.resourceLoadTime)}
+          </div>
+          <div>
+            Element render delay:{" "}
+            {Math.round(lcp.attribution.elementRenderDelay)}
+          </div>
         </>
       )}
     </div>
