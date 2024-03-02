fb50e53eb87c831239903e1cc5d85e810905bc4c
張文源 <c29429768@gmail.com>
2024-03-02 10:32:47 +0800

    Add prop 'collapse' to CodePreview

--- src/app/components/CodePreview/index.tsx
+++ src/app/components/CodePreview/index.tsx
@@ -1,10 +1,35 @@
+"use client";
+
+import { Button, Collapse } from "@material-tailwind/react";
 import type { FC } from "react";
+import { useState } from "react";
 import SyntaxHighlighter, {
   SyntaxHighlighterProps,
 } from "react-syntax-highlighter";
 import { stackoverflowLight } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-const CodePreview: FC<SyntaxHighlighterProps> = (props) => {
+interface CodePreviewProps extends SyntaxHighlighterProps {
+  collapse?: boolean;
+}
+
+const CodePreview: FC<CodePreviewProps> = ({ collapse = false, ...props }) => {
+  const [openState, setOpenState] = useState(false);
+
+  const toggleOpen = () => setOpenState((cur) => !cur);
+
+  if (collapse) {
+    return (
+      <div>
+        <Button className="my-4" color="cyan" onClick={toggleOpen}>
+          view code
+        </Button>
+        <Collapse open={openState}>
+          <SyntaxHighlighter style={stackoverflowLight} {...props} />
+        </Collapse>
+      </div>
+    );
+  }
+
   return <SyntaxHighlighter style={stackoverflowLight} {...props} />;
 };
 
