48896c81bab9bc89ce86c01838fddd3c0b4b93c0
張文源 <c29429768@gmail.com>
2024-03-11 21:34:20 +0800

    Fix ul in p

--- src/app/(page)/page.tsx
+++ src/app/(page)/page.tsx
@@ -3,23 +3,21 @@ import ExternalLink from "@/app/components/ExternalLink";
 export default function Home() {
   return (
     <div>
-      <p>
-        這是將我從 web.dev 網站
-        <ul>
-          <li>
-            <ExternalLink href="https://web.dev/learn/performance/welcome">
-              https://web.dev/learn/performance/welcome
-            </ExternalLink>
-          </li>
-          <li>
-            <ExternalLink href="https://web.dev/explore/learn-core-web-vitals">
-              https://web.dev/explore/learn-core-web-vitals
-            </ExternalLink>
-          </li>
-        </ul>
-        上所學到的知識整理而成的筆記。
-      </p>
-
+      <p>這是將我從 web.dev 網站</p>
+      <ul>
+        <li>
+          <ExternalLink href="https://web.dev/learn/performance/welcome">
+            https://web.dev/learn/performance/welcome
+          </ExternalLink>
+        </li>
+        <li>
+          <ExternalLink href="https://web.dev/explore/learn-core-web-vitals">
+            https://web.dev/explore/learn-core-web-vitals
+          </ExternalLink>
+        </li>
+      </ul>
+      <p>上所學到的知識整理而成的筆記。</p>
+      <br />
       <p>
         內容涵蓋了提升網站性能和優化核心 Web 關鍵指標 (Core Web Vitals)
         所需的資訊，以及測試用的範例。
